67ce320b9b53dcac3fd9ae45c006b41a1c81d3fe
rick walker <=>
2016-08-23 17:46:05 -0400

    Changed PathValidator class to use the Files class instead of File in perform path checking

--- pathvalidator/src/main/java/com/gmail/ichglauben/pathvalidator/core/concretes/PathValidator.java
+++ pathvalidator/src/main/java/com/gmail/ichglauben/pathvalidator/core/concretes/PathValidator.java
@@ -1,35 +1,45 @@
 package com.gmail.ichglauben.pathvalidator.core.concretes;
 
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class PathValidator {
+	/**Validates the path exists and is not a symbolic link.
+	 * @param filePath - String file or directory path
+	 * @return true if path exists and is not a symbolic link, false if otherwise*/
 	public static boolean pathExists(String filePath) {
 		Path path = null;
 		if (null != filePath) {
 			if (null != (path = Paths.get(filePath))) {
-				return (isADirectory(path.toAbsolutePath().toString()) || isAFile(path.toAbsolutePath().toString()));
+				return Files.exists(path, LinkOption.NOFOLLOW_LINKS);
 			}
 		}
 		return false;
 	}
 	
+	/**Validates the directory exists, it's not a symbolic link and that it is a directory.
+	 * @param filePath - String
+	 * @return true if directory exists and is not a symbolic link, false if otherwise*/
 	public static boolean isADirectory(String filePath) {
 		Path path = null;
 		if (null != filePath) {
 			if (null != (path = Paths.get(filePath))) {
-				return (path.toFile().exists() && path.toFile().isDirectory() && Files.isWritable(path));
+				return (Files.exists(path, LinkOption.NOFOLLOW_LINKS) && Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS));
 			}
 		}
 		return false;
 	}
 	
+	/**Validates the file exists, it's not a symbolic link and that it is a file.
+	 * @param filePath - String
+	 * @return true if file exists and is not a symbolic link, false if otherwise*/
 	public static boolean isAFile(String filePath) {
 		Path path = null;
 		if (null != filePath) {
 			if (null != (path = Paths.get(filePath))) {
-				return (path.toFile().exists() && path.toFile().isFile());
+				return (Files.exists(path, LinkOption.NOFOLLOW_LINKS) && Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS));
 			}
 		}
 		return false;
